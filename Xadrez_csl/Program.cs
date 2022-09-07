5abf89b284eb5fc6d1d211f4208b2bd81dcc856d
Lucas Silva <lucasdjs2014@hotmail.com>
2022-09-06 22:09:04 -0300

    Melhorando implementação do tabuleiro

--- Xadrez_csl/Program.cs
+++ Xadrez_csl/Program.cs
@@ -8,8 +8,10 @@ try {
 
 
     tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 9));
+    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 4));
     tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+
+    tab.ColocarPeca(new Torre(Cor.Branca,tab), new Posicao(3, 5));
 
     Tela.ImprimirTabuleiro(tab);
 
--- Xadrez_csl/Tela.cs
+++ Xadrez_csl/Tela.cs
@@ -8,6 +8,7 @@ namespace Xadrez_csl
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8-i + " ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -15,11 +16,29 @@ namespace Xadrez_csl
                         Console.Write("- ");
                     }
                     else {
-                        Console.Write(tab.peca(i, j) + " "); 
+                        Tela.imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                     
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+
+        }
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
 
         }
